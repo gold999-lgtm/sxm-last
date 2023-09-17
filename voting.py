78c647390c46aae9ecaa77e6a6bf7890d8faa927
gold999-lgtm <72864295+gold999-lgtm@users.noreply.github.com>
2023-09-17 20:01:44 +0530

    Update voting.py

--- voting.py
+++ voting.py
@@ -13,31 +13,9 @@ import plotly.express as px
 
 # Function to visualize data
 def visualize(m, n):
-    st.write(f"Generating data for {m} users and {n} categories...")
+    
 
-    # Create a DataFrame
-    df = pd.DataFrame()
-    df["UserId"] = list(range(1, m + 1))
-    for j in range(1, n + 1):
-        df["Channel" + str(j)] = np.random.randint(-1, 2, df.shape[0])
-
-    st.write(f"Data Shape: {df.shape}")
-    st.write(f"Data Types: {type(df.iloc[:, 0])}, {type(df)}")
-
-    st.write("Data Columns with Null Values:")
-    st.write(df.isnull().any(axis=0))
-
-    st.write("Rows with Null Values:")
-    st.write(df.isnull().any(axis=1))
-
-    st.write("Saving data to CSV...")
-    df.to_csv("Voting Data.csv", index=False)
-
-    st.write("Creating SQLite database and table...")
-    conn = sql.connect("voting_data.db")
-    df.to_sql("ChannelVotingTable53", conn, index=False)
-
-    st.write("Reading data from SQLite database...")
+    
     conn = sql.connect("voting_data.db")
     df = pd.read_sql_query("SELECT * from ChannelVotingTable53", conn)
 
